8546b936b0eac95f5fd15899e0f3b827115a3968
AbdelwahabYoussef <66198057+AbdelwahabYoussef@users.noreply.github.com>
2022-08-04 10:54:02 +0200

    Update 0001_initial.py

--- reviews/migrations/0001_initial.py
+++ reviews/migrations/0001_initial.py
@@ -11,6 +11,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
+        ('tags', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('auth', '0012_alter_user_first_name_max_length'),
     ]

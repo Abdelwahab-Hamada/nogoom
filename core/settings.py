452d750ef8d5e357f2e4adb8a3cd1b1e9a406955
AbdelwahabYoussef <66198057+AbdelwahabYoussef@users.noreply.github.com>
2022-08-06 15:37:04 +0200

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -205,11 +205,11 @@ SIMPLE_JWT = {
 
 SIMPLE_JWT.update({# custom
   'AUTH_COOKIE': 'refresh_token',  # Cookie name. Enables cookies if value is set.
-  'AUTH_COOKIE_DOMAIN': 'abdelwahabyoussef.github.io',     # A string like "example.com", or None for standard domain cookie.
+  'AUTH_COOKIE_DOMAIN': None,     # A string like "example.com", or None for standard domain cookie.
   'AUTH_COOKIE_SECURE': False,    # Whether the auth cookies should be secure (https:// only).
   'AUTH_COOKIE_HTTP_ONLY' : True, # Http only cookie flag.It's not fetch by javascript.
   'AUTH_COOKIE_PATH': '/',        # The path of the auth cookie.
-  'AUTH_COOKIE_SAMESITE': None,  # Whether to set the flag restricting cookie leaks on cross-site requests. This can be 'Lax', 'Strict', or None to disable the flag.
+  'AUTH_COOKIE_SAMESITE': 'Lax',  # Whether to set the flag restricting cookie leaks on cross-site requests. This can be 'Lax', 'Strict', or None to disable the flag.
 })
 
 STATIC_URL = '/static/'

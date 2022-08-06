34cd48f319e89c56d5910be19921fa6a1e3f5eb4
AbdelwahabYoussef <66198057+AbdelwahabYoussef@users.noreply.github.com>
2022-08-06 19:33:34 +0200

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -163,12 +163,16 @@ INTERNAL_IPS= [
 
 
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
+    'https://abdelwahabyoussef.github.io',
+]
+
 from corsheaders.defaults import default_headers
 CORS_ALLOW_HEADERS = default_headers + (
     'Access-Control-Allow-Origin',
 )
-
 
 CORS_ALLOW_CREDENTIALS = True
 

8ea91171716cdf1f2c37780c0cf75f95ba900011
AbdelwahabYoussef <66198057+AbdelwahabYoussef@users.noreply.github.com>
2022-08-08 17:39:54 +0200

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -147,7 +147,7 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticatedOrReadOnly"
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (     
-        'rest_framework.authentication.BasicAuthentication',   
+        #'rest_framework.authentication.BasicAuthentication',   
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         
